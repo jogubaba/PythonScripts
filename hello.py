272c6ac0ae6b756ea11b0461f5ec9feb7d218b11
jogubaba <baba.jogu510@gmail.com>
2021-04-22 11:00:02 +0530

    New Hello World with good Pylint

--- hello.py
+++ hello.py
@@ -2,5 +2,5 @@
 '''This is a small python script to print Hello'''
 
 print('Hello World!')
-NAME = input('What is your name? : ')
+NAME = input('What is your name? :')
 print('Welcome to this IT World ' + NAME)
